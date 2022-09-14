0b85b3f717e6563ff4b1fac99843ba1c99ccf6ab
devika0999 <112818841+devika0999@users.noreply.github.com>
2022-09-14 13:12:24 +0530

    Update Employe.java

--- Employe.java
+++ Employe.java
@@ -1,26 +1,15 @@
 import java.util.*;
 public class Employe
 {
-
-public static float total(float ass1,float ass2,float ass3)
-{     
-	float total= ass1+ass2+ass3;
-	return total;
-}
-public static float percentage (float ass1,float ass2,float ass3)
-{     
-	float total =ass1+ass2+ass3;
-	float per= (total/300)*100;
-	return per;
-}
-
-public static void main(String args[])
-{
-	String emp_name,result;
+String emp_name,result;
 	int emp_id;
 	float ass1 ,ass2, ass3, per ,total;
+
+Employe()
+{
+
 	
-	Employe emp =new Employe();
+	
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter employee name and id");
 	emp_name= sc.nextLine();
@@ -30,21 +19,50 @@ public static void main(String args[])
 	ass1=sc.nextFloat();
 	ass2=sc.nextFloat();
 	ass3=sc.nextFloat();
-
 	System.out.println("Employee Name ="+emp_name+" "+"Employee ID ="+emp_id);
-	System.out.println("Employee total marks="+emp.total(ass1,ass1,ass3)+"/300");
-	System.out.println("Employee Percentage="+emp.percentage(ass1,ass1,ass3));
+}
+
+void total()
+{     
+	float result= ass1+ass2+ass3;
+	System.out.println("Employee total marks="+result+"/300");
+}
+void percentage ()
+{     
+	float total =ass1+ass2+ass3;
+	float per= (total/300)*100;
+	System.out.println("Employee Percentage="+per);
+}
+
+
+
+void condition ()
+{	
+	
+	
 	if(ass1>=75 && ass2>=75 && ass3>=75)
 	{
-		System.out.println("promoted");
-
+		 System.out.println("promoted");
+		
 	}
 	else
 	{
-		System.out.println("demoted");
-
+	 	System.out.println("demoted");
+		
 	}
+}
 
 
+public static void main(String args[])
+{
+	Employe emp =new Employe();
+
+	emp.total();
+	emp.percentage();
+	emp.condition();
+
+	
+	
+	
 }	
 }
